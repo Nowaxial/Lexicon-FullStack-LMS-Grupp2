ea3ae6b707398afab5916ca10a90fe5890caf371
NiklasMattsson <niklasmattson1@gmail.com>
2025-08-19 16:26:22 +0200

    DbContext/Entities/BigTask

---
+++ Domain.Models/Entities/CourseUser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models.Entities
+{
+    public class CourseUser
+    {
+        public string UserId { get; set; } = null!; // FK to AspNetUser
+        public int CourseId { get; set; }
+        public bool IsTeacher { get; set; }
+
+        // Navigation
+        public Course Course { get; set; } = null!;
+    }
+}
