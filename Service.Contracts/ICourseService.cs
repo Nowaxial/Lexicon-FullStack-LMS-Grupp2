a5534dfd20d8f2495226d3dc3ce39848b6f62ad5
NiklasMattsson <niklasmattson1@gmail.com>
2025-08-20 17:53:15 +0200

    feat(service): add ICourseService interface

---
+++ Service.Contracts/ICourseService.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Models.Entities;
+
+namespace Service.Contracts;
+
+public interface ICourseService
+{
+    Task CreateCourseAsync(Course course);
+    Task DeleteCourseAsync(Course course);
+    Task<IEnumerable<Course>> GetAllCoursesAsync(bool trackChanges = false);
+    Task<Course?> GetCourseByIdAsync(int id, bool trackChanges = false);
+    Task UpdateCourseAsync(Course course);
+}
