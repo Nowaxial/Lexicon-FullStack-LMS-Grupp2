c72bd21a0529ecaef847b96d1c279e7d8b53d941
NiklasMattsson <niklasmattson1@gmail.com>
2025-08-20 17:52:03 +0200

    feat(repository): implement CourseRepository

---
+++ LMS.Infractructure/Repositories/CourseRepository.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Contracts.Repositories;
+using Domain.Models.Entities;
+using LMS.Infractructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMS.Infractructure.Repositories
+{
+    public class CourseRepository : RepositoryBase<Course>, ICourseRepository
+    {
+        public CourseRepository(ApplicationDbContext context) : base(context) { }
+
+        public async Task<Course?> GetCourseWithModulesAsync(int id, bool trackChanges = false)
+        {
+            return await FindByCondition(c => c.Id == id, trackChanges)
+                            .Include(c => c.Modules)
+                            .FirstOrDefaultAsync();
+        }
+    }
+}
