b82f7cf7a11eff69e23b108f847c12a77169c136
NiklasMattsson <niklasmattson1@gmail.com>
2025-08-20 08:31:49 +0200

    Created entity course.

---
+++ Domain.Models/Entities/Course.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models.Entities
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string? Description { get; set; }
+        public DateOnly Starts { get; set; }
+        public DateOnly Ends { get; set; }
+    }
+}
