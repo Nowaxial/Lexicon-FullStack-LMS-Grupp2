39318fd12226e633bff0c826bea32a7ed1ff4eb5
NiklasMattsson <niklasmattson1@gmail.com>
2025-08-20 17:51:21 +0200

    feat(repository): add ICourseRepository interface

---
+++ Domain.Contracts/Repositories/ICourseRepository.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Contracts.Repositories
+{
+    public interface ICourseRepository : IRepositoryBase<Course>, IInternalRepositoryBase<Course>
+    {
+        Task<Course?> GetCourseWithModulesAsync(int id, bool trackChanges = false);
+    }
+}
