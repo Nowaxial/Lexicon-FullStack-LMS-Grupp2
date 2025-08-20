9805725de47dddf5c27c3dc4ba21f3ad11c617eb
OlenaKut <cundrochka@gmail.com>
2025-08-20 11:05:46 +0200

    Code behind

---
+++ LMS.Blazor/Components/Layout/NavMenu.razor.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Routing;
+
+namespace LMS.Blazor.Components.Layout
+{
+    public partial class NavMenu
+    {
+        private string? currentUrl;
+        protected override void OnInitialized()
+        {
+            currentUrl = NavigationManager.ToBaseRelativePath(NavigationManager.Uri);
+            NavigationManager.LocationChanged += OnLocationChanged;
+        }
+        private void OnLocationChanged(object? s, LocationChangedEventArgs e)
+        {
+            currentUrl = NavigationManager.ToBaseRelativePath(e.Location);
+            StateHasChanged();
+        }
+        public void Dispose() => NavigationManager.LocationChanged -= OnLocationChanged;
+    }
+}
