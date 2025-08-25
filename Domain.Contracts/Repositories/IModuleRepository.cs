d815eceba31894a03e5a425952c6f30f1fca128a
OlenaKut <cundrochka@gmail.com>
2025-08-25 12:34:24 +0200

    Added ModuleRepository and IModuleRepository

---
+++ Domain.Contracts/Repositories/IModuleRepository.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Contracts.Repositories
+{
+    public interface IModuleRepository : IRepositoryBase<Module>
+    {
+        Task<IEnumerable<Module>> GetAllAsync(bool trackChanges = false);
+        Task<Module?> GetByIdAsync(int id, bool trackChanges = false);
+        Task<IEnumerable<Module>> GetByCourseAsync(int courseId, bool trackChanges = false);
+    }
+}
