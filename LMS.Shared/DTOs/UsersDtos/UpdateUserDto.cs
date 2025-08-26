893624d0b490373dc5930cf4cd5bec14399051f3
NiklasMattsson <niklasmattson1@gmail.com>
2025-08-26 17:00:56 +0200

    Added basic dtos for users. And for api paging.

---
+++ LMS.Shared/DTOs/UsersDtos/UpdateUserDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMS.Shared.DTOs.UsersDtos
+{
+    public class UpdateUserDto
+    {
+        public string? FullName { get; init; }
+        public string? Email { get; init; }
+        public string? UserName { get; init; }
+    }
+}
---
+++ LMS.Shared/DTOs/UsersDtos/UserDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMS.Shared.DTOs.UsersDtos
+{
+    public class UserDto
+    {
+        public required string Id { get; init; }
+        public string? FullName { get; init; }
+        public string? UserName { get; init; }
+        public string? Email { get; init; }
+    }
+}
